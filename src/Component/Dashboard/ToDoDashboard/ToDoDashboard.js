14c589394b3b657f225a43b81bde201763c41921
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-06-25 01:10:52 +0600

    50% done with mobile device responsive

--- src/Component/Dashboard/ToDoDashboard/ToDoDashboard.js
+++ src/Component/Dashboard/ToDoDashboard/ToDoDashboard.js
@@ -139,16 +139,16 @@ const ToDoDashboard = () => {
                 </div>
 
             </div>
-            {/* tab covid banner  */}
-            <div className='rounded-xl shadow-lg my-3 p-3 covid-for-tab covid-md-width'>
-                <div className='flex justify-between'>
-                    <h6>Upcoming Appointments</h6>
+            {/* tab & mobile covid banner  */}
+            <div className='rounded-xl shadow-lg my-3 p-3 covid-for-tab covid-md-width covid-sm-sizing covid-sm-width'>
+                <div className='flex justify-between covid-sm-sizing'>
+                    <h6>Covid-19 Updates</h6>
                     <div>
                         <h6>10 September 2022</h6>
                         <h6 className='font-thin text-gray-300'>Thursday  10:00:00 AM</h6>
                     </div>
                 </div>
-                <div className='flex my-4 justify-around'>
+                <div className='flex my-4 justify-around covid-sm-sizing'>
                     <div>
                         <svg width="228" height="228" viewBox="0 0 228 228" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M29.339 55.3037L28.427 54.0634C28.386 54.0041 28.5866 53.6575 28.5228 53.58C28.4589 53.5025 27.6108 53.8445 27.7157 54.0086C27.8205 54.1728 28.0531 54.1318 28.0805 54.1774C28.1078 54.223 28.728 55.632 28.728 55.632L29.339 55.3037Z" fill="#EBEBEB" />
@@ -1007,14 +1007,14 @@ const ToDoDashboard = () => {
                             <path d="M109.463 59.6859L102.235 52.3716L104.666 49.9731L108.888 54.2458L117.657 40.4974L120.535 42.3305L109.463 59.6859Z" fill="#FF7C65" />
                         </svg>
                     </div>
-                    <div className='flex rounded-md text-white'>
-                        <div className='Linear p-3 h-52'>
+                    <div className='flex rounded-md text-white covid-sm-sizing'>
+                        <div className='Linear p-3 h-52 covid-sm-sizing'>
                             <p>Infection Number </p>
                             <h3>500</h3>
                             <p>Infection Rate </p>
                             <h4>10%</h4>
                         </div>
-                        <div className='Linear p-3 h-52'>
+                        <div className='Linear p-3 h-52 covid-sm-sizing'>
                             <p>Infection Number </p>
                             <h3>500</h3>
                             <p>Infection Rate </p>
